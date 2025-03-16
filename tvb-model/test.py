dc73f1f640521d6981e52ec142522b29edeb54be
sngmin <lsgmin@naver.com>
2025-03-17 08:10:13 +0900

    test image

--- tvb-model/test.py
+++ tvb-model/test.py
@@ -8,7 +8,7 @@ loaded_model = tf.keras.models.load_model('xception_model-2.h5')
 image_path = "ex/t.png"  # 테스트할 이미지 파일 경로
 
 # 1. 이미지 로드 및 전처리
-img_size = (224, 224)  # 모델이 학습한 입력 크기에 맞춰야 함
+img_size = (256, 256)  # 모델이 학습한 입력 크기에 맞춰야 함
 img = load_img(image_path, target_size=img_size)  # 이미지 불러오기 및 크기 조정
 img_array = img_to_array(img)  # numpy 배열 변환
 img_array = np.expand_dims(img_array, axis=0)  # 배치 차원 추가 (1, 224, 224, 3)
