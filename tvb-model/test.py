0c7486a4190a2e2f8558662db7fcf953c6ff5626
sngmin <lsgmin@naver.com>
2025-03-17 08:08:37 +0900

    test image

--- tvb-model/test.py
+++ tvb-model/test.py
@@ -1,13 +1,30 @@
+import numpy as np
 import tensorflow as tf
+from keras.src.utils import load_img, img_to_array
+
 from data_loader import get_testData_generators
 loaded_model = tf.keras.models.load_model('xception_model-2.h5')
 
-test = get_testData_generators()
+image_path = "ex/t.png"  # 테스트할 이미지 파일 경로
 
-predictions = loaded_model.predict(test)
-loss, accuracy = loaded_model.evaluate(test)
+# 1. 이미지 로드 및 전처리
+img_size = (224, 224)  # 모델이 학습한 입력 크기에 맞춰야 함
+img = load_img(image_path, target_size=img_size)  # 이미지 불러오기 및 크기 조정
+img_array = img_to_array(img)  # numpy 배열 변환
+img_array = np.expand_dims(img_array, axis=0)  # 배치 차원 추가 (1, 224, 224, 3)
+img_array = img_array / 255.0  # 정규화 (모델 학습 시 정규화했다면 필요)
 
-print(f"Test loss: {loss}")
-print(f"Test accuracy: {accuracy}")
+prediction = loaded_model.predict(img_array)
 
-print(predictions)
+# 3. 결과 출력
+print(f"Prediction: {prediction}")
+
+# test = get_testData_generators()
+
+# predictions = loaded_model.predict(test)
+# loss, accuracy = loaded_model.evaluate(test)
+#
+# print(f"Test loss: {loss}")
+# print(f"Test accuracy: {accuracy}")
+#
+# print(predictions)
